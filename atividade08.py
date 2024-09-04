96051625ff8e2d6ae42502539e7cba280019d6b0
Kaliandrik <contatokaliandrik@gmail.com>
2024-09-04 16:01:45 -0300

    Converter real em dólar

---
+++ atividade08.py
@@ -0,0 +1,10 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+
+
+valorreal = int(input("Digite seu valor em R$: "))
+
+conversao = valorreal /5.63
+
+
+print("Seu valor em dólar é: ", conversao)
+
